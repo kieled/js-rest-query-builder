611261a954691bd0c6adb4365ea58acc93912134
kieled <68655454+kieled@users.noreply.github.com>
2022-05-26 19:19:50 +0300

    Uploaded

---
+++ main.ts
@@ -0,0 +1,56 @@
+const buildQuery = (props: fetchGroupsProps, filters: string[], search: string[], ordering: string[]) => {
+    let query = '?'
+
+    if (filters.length) {
+        for (const [key, value] of Object.entries(props)) {
+            if (key in filters && value) {
+                query += key + '=' + value + '&'
+            }
+        }
+    }
+
+    if (search.length) {
+        let count = 0
+        for (const [key, value] of Object.entries(props)) {
+            if (key in ordering && value !== '') {
+                count += 1
+            }
+        }
+        if (count > 0) {
+            query += 'search='
+            for (const [key, value] of Object.entries(props)) {
+                if (key in search && value !== '') {
+                    query += value + ','
+                }
+            }
+        }
+        if (query[-1] === ',') {
+            query = query.slice(0, -1)
+        }
+        if (query[-1] !== '&') {
+            query += '&'
+        }
+    }
+
+    if (ordering.length) {
+        let count = 0
+        for (const [key, value] of Object.entries(props)) {
+            if (key in ordering && value !== '') {
+                count += 1
+            }
+        }
+        if (count > 0) {
+            query += 'ordering='
+            for (const [key, value] of Object.entries(props)) {
+                if (key in ordering && value !== '') {
+                    query += value + ','
+                }
+            }
+        }
+        if (query[-1] == ',') {
+            query = query.slice(0, -1)
+        }
+    }
+
+    return query
+}
